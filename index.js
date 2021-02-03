c1a1882a509c667528833fdfb5c710b35367dcb2
Jordan <jll541@york.ac.uk>
2021-02-03 13:25:04 +0000

    mongodb cloud

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 var express = require('express')
 var app = express()
 
-const mongoose = require('mongoose');
-const config = require('./config/database');
-const path = require('path');
+// const mongoose = require('mongoose');
+// const config = require('./config/database');
+// const path = require('path');
 
-mongoose.Promise = global.Promise;
-mongoose.connect(config.uri, {useNewUrlParser: true}, (err) => {
-    if (err) {
-        console.log('Could NOT connect to DB', err);
-    } else {
-        // console.log('Crypto', config.secret);
-        console.log('Connected to DB', config.db);
-    }
+// mongoose.Promise = global.Promise;
+// mongoose.connect(config.uri, {useNewUrlParser: true}, (err) => {
+//     if (err) {
+//         console.log('Could NOT connect to DB', err);
+//     } else {
+//         // console.log('Crypto', config.secret);
+//         console.log('Connected to DB', config.db);
+//     }
+// });
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://jll541:<kin6ungh>@clusterquiz.inacn.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
 });
+
 
 app.use(express.static(__dirname + '/quiz-platform/dist/quiz-platform'));
 app.get('*', (req, res) => {
