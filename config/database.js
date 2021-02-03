97d51dbb261d0e55acc7d359cf11eba4a77c4107
Jordan <jll541@york.ac.uk>
2021-02-03 14:08:47 +0000

    change localhost to 0.0.0.0

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const crypto = require('crypto').randomBytes(256).toString('hex');
 
 module.exports = {
-    uri: 'mongodb://localhost:27017/' + this.db,
+    uri: 'mongodb://0.0.0.0:27017/' + this.db,
     // encryption for tokens and to know its our db
     secret: crypto,
     db: 'mean-quiz'
