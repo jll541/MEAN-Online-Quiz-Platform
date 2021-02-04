20e0f3e778e22d4e2aa2162c60c4491383e9837b
Jordan <jll541@york.ac.uk>
2021-02-04 18:32:16 +0000

    remove build

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ const path = require('path');
 
 app.use(express.static('../build'));
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/', '../build', 'index.html' ));
+    res.sendFile(path.join(__dirname, '/', 'index.html' ));
   })
   
     const PORT = process.env.PORT || 8080;
