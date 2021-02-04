38b9546ccafb3aa6d98e8129b4cf1764520514e6
Jordan <jll541@york.ac.uk>
2021-02-04 20:28:16 +0000

    added title

--- quiz-platform/src/app/app.module.ts
+++ quiz-platform/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TitlepageComponent } from './titlepage/titlepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitlepageComponent
   ],
   imports: [
     BrowserModule
