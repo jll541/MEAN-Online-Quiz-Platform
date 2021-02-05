8a4aa4437042f72a200fe01e46384f79f1d56e2f
Jordan <jll541@york.ac.uk>
2021-02-05 16:02:51 +0000

    added engines

---
+++ server.js
@@ -0,0 +1,10 @@
+// var express = require('express')
+// var app = express()
+
+// app.use(express.static('./dist/mean-online-quiz-platform.json>'));
+// app.get('/*', function(req, res) {
+//     res.sendFile('index.html', {root: 'dist/mean-online-quiz-platform.json>/'}
+//   );
+//   });
+
+// app.listen(process.env.PORT || 8080);
